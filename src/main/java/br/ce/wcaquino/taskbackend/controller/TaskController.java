c79a01e41d30e55e0ebfbd33512969e73694e01a
danie <daniel.freitas.ms1@gmail.com>
2020-06-05 12:06:10 -0300

    fix delete @pathVariable

--- src/main/java/br/ce/wcaquino/taskbackend/controller/TaskController.java
+++ src/main/java/br/ce/wcaquino/taskbackend/controller/TaskController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,7 +49,7 @@ public class TaskController {
 	}
 	
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> delete (@RequestParam Long id){
+	public ResponseEntity<Void> delete (@PathVariable Long id){
 		todoRepo.deleteById(id);
 		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
